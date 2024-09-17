eded7e0a0e68740855c9400ec56e9efe9aaf0a29
Developer Shakil <itsdevelopershakil@gmail.com>
2024-09-17 19:43:59 +0600

    Added Socket Message event on newConnectionHandler.ts

--- src/socket/handlers/newConnectionHandler.ts
+++ src/socket/handlers/newConnectionHandler.ts
@@ -1,18 +1,22 @@
-import { Socket } from 'socket.io';
-import { print } from '../../helper/colorConsolePrint.ts/colorizedConsole';
-import socketStore from '../socket-store';
+import { Socket } from "socket.io";
+import { print } from "../../helper/colorConsolePrint.ts/colorizedConsole";
+import socketStore from "../socket-store";
 
 const newConnectionHandler = async (socket: Socket, io: any) => {
-    print.green('user connected 💥' + socket.id);
-    const userDetails = socket.user;
+  print.green("user connected 💥" + socket.id);
+  const userDetails = socket.user;
+  console.log(userDetails);
 
-    socketStore.addNewConnectedUser({
-        socketId: socket.id,
-        userId: userDetails?.id,
-        role: userDetails?.role,
-    });
+  socket.on("message", (msg) => {
+    console.log("Message received: " + JSON.stringify(msg));
+    io.emit("message", msg); // Broadcast message to all connected clients
+  });
+
+  socketStore.addNewConnectedUser({
+    socketId: socket.id,
+    userId: userDetails?.id,
+    role: userDetails?.role,
+  });
 };
 
 export default newConnectionHandler;
-
- 
