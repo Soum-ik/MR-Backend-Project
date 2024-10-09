92cbab49f4dbc2b603ec7b5990db667ad08fdf97
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-10-09 21:33:03 +0600

    typing socket added

--- src/socket/handlers/newConnectionHandler.ts
+++ src/socket/handlers/newConnectionHandler.ts
@@ -11,6 +11,14 @@ const newConnectionHandler = async (socket: Socket, io: any) => {
     io.emit("message", msg); // Broadcast message to all connected clients
   });
 
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('displayTyping', data);
+  });
+
+  socket.on('stopTyping', (data) => {
+    socket.broadcast.emit('hideTyping', data);
+  });
+
   socketStore.addNewConnectedUser({
     socketId: socket.id,
     userId: userDetails?.user_id,
