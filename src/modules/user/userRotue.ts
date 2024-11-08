2c06d4f9825d576bd0dd68127b2c0aa8e4071005
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-11-08 16:17:00 +0600

    get user by id added

--- src/modules/user/userRotue.ts
+++ src/modules/user/userRotue.ts
@@ -1,18 +1,28 @@
-import express from "express";
-import { User } from "./userController";
-import authenticateToken from "../../middleware/auth";
-import { USER_ROLE } from "./user.constant";
+import express from 'express';
+import authenticateToken from '../../middleware/auth';
+import { USER_ROLE } from './user.constant';
+import { User } from './userController';
 
 const router = express.Router();
 
 // get users api route -done
-router.post("/sign-up", User.SignUp);
-router.post("/sign-in", User.SignIn);
-router.get("/forgot-pass/:email", User.forgotPass);
-router.get("/verify-otp/:email", User.verifyOtp);
-router.put("/set-forget-pass/:forgetPasswordToken", User.setForgetNewPass);
-router.put("/set-new-pass/", authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER), User.setNewPass);
-router.get("/all-user/", User.getAllUser);
-router.post("/update-user/", User.updateUser);
+router.post('/sign-up', User.SignUp);
+router.post('/sign-in', User.SignIn);
+router.get('/forgot-pass/:email', User.forgotPass);
+router.get('/verify-otp/:email', User.verifyOtp);
+router.put('/set-forget-pass/:forgetPasswordToken', User.setForgetNewPass);
+router.put(
+  '/set-new-pass/',
+  authenticateToken(
+    USER_ROLE.ADMIN,
+    USER_ROLE.SUPER_ADMIN,
+    USER_ROLE.SUB_ADMIN,
+    USER_ROLE.USER,
+  ),
+  User.setNewPass,
+);
+router.get('/all-user/', User.getAllUser);
+router.post('/update-user/', User.updateUser);
+router.get('/getUserById/:id', User.getUserById);
 
 export const UserRoute = router;
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,10 +2,21 @@ import express from 'express';
 import UserController from '../controller/UserController';
 import SocialMediaLinkController from '../controller/socialMediaLinkController';
 
+import httpStatus from 'http-status';
+import { AWS_SES } from '../helper/smtp/AWS_SES';
+import sendResponse from '../libs/sendResponse';
+import catchAsync from '../libs/utlitys/catchSynch';
 import authenticateToken from '../middleware/auth';
 import authenticateSuperAdmin from '../middleware/super_admin_auth';
 import { uploadAttachmentToS3AndFormatBody } from '../middleware/uploadAttachmentToS3AndFormatBody';
+import { uploadAttachmentToS3AndFormatBodyOptimized } from '../middleware/uploadAttachmentToS3AndFormatBodyOptimized';
 import { uploadFile } from '../middleware/uploadFileWihtMulter';
+import OrderNoteRouter from '../modules/Order_page/Note/OrderNote.rotue';
+import { handleOrderMessageRoute } from '../modules/Order_page/Order-message/Order-message.route';
+import getOrderStatusRoute from '../modules/Order_page/Order-status/order-status.route';
+import findOrderRouter from '../modules/Order_page/Order_page.Route';
+import { RequirementSubmitRoute } from '../modules/Order_page/Requirement/Requirement.route';
+import { Start_Project_Controller } from '../modules/Order_page/Start_project/Start_project.route';
 import { CategoryRoute } from '../modules/ProjectCategory/CategoryRoute';
 import { quickResponseRouter } from '../modules/QuickResponses/quickResponses.router';
 import { DesignsRoute } from '../modules/Upload-design/designs/designs.route';
@@ -20,36 +31,35 @@ import { blockChatRouter } from '../modules/chat/block_chat/block_chat.route';
 import { chating } from '../modules/chat/chat.controller';
 import { handleNotificationRoute } from '../modules/chat/get_notification/get_notification.router';
 import { handleMessageRoute } from '../modules/chat/user-admin-converstion/user-admin.router';
-import { startContact } from "../modules/contact/contact.controller";
+import { startContact } from '../modules/contact/contact.controller';
 import { createProjectRoute } from '../modules/create-project-admin/createProject.route';
 import { multiProjectRoute } from '../modules/multi-project/multiProject.route';
 import { payment } from '../modules/payment/payment.controller';
 import { handleRoleRoute } from '../modules/role_controller_super_admin/role_controller.router';
-import uploadImage from '../modules/uploadImage/uploadController';
-import { UserRoute } from '../modules/user/userRotue';
-import { Start_Project_Controller } from '../modules/Order_page/Start_project/Start_project.route';
-import { USER_ROLE } from '../modules/user/user.constant'
 import { sendMessageForChat } from '../modules/send_message_from_admin/sendMessage.controller';
-import { AWS_SES } from '../helper/smtp/AWS_SES';
-import { handleOrderMessageRoute } from '../modules/Order_page/Order-message/Order-message.route';
-import getOrderStatusRoute from '../modules/Order_page/Order-status/order-status.route';
-import { uploadAttachmentToS3AndFormatBodyOptimized } from '../middleware/uploadAttachmentToS3AndFormatBodyOptimized';
-import findOrderRouter from '../modules/Order_page/Order_page.Route';
-import OrderNoteRouter from '../modules/Order_page/Note/OrderNote.rotue';
-import { RequirementSubmitRoute } from '../modules/Order_page/Requirement/Requirement.route';
-import catchAsync from '../libs/utlitys/catchSynch';
-import httpStatus from 'http-status';
-import sendResponse from '../libs/sendResponse';
+import uploadImage from '../modules/uploadImage/uploadController';
+import { USER_ROLE } from '../modules/user/user.constant';
+import { UserRoute } from '../modules/user/userRotue';
 
 const router = express.Router();
 router.get(
   '/social-media-link/',
-  authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER),
+  authenticateToken(
+    USER_ROLE.ADMIN,
+    USER_ROLE.SUPER_ADMIN,
+    USER_ROLE.SUB_ADMIN,
+    USER_ROLE.USER,
+  ),
   SocialMediaLinkController.getSocialMediaLinks,
 );
 router.post(
   '/social-media-link/',
-  authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER),
+  authenticateToken(
+    USER_ROLE.ADMIN,
+    USER_ROLE.SUPER_ADMIN,
+    USER_ROLE.SUB_ADMIN,
+    USER_ROLE.USER,
+  ),
   SocialMediaLinkController.upsertSocialMediaLink,
 );
 // middleware applyed
@@ -59,14 +69,12 @@ router.post(
   uploadFile.array('files'),
   uploadAttachmentToS3AndFormatBody(),
   catchAsync((req, res) => {
-
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Attachment uploaded and processed successfully',
       data: req.body,
     });
-
   }),
 );
 
@@ -75,20 +83,23 @@ router.post(
   uploadFile.array('files'),
   uploadAttachmentToS3AndFormatBodyOptimized(),
   catchAsync((req, res) => {
-
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Attachment uploaded and processed successfully',
       data: req.body,
     });
-
   }),
 );
 
 router.get(
   '/get-singel-user/',
-  authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER),
+  authenticateToken(
+    USER_ROLE.ADMIN,
+    USER_ROLE.SUPER_ADMIN,
+    USER_ROLE.SUB_ADMIN,
+    USER_ROLE.USER,
+  ),
   UserController.getSingelUser,
 );
 
@@ -101,28 +112,50 @@ router.use('/industrys', IndustrysRoute);
 router.use('/designs', DesignsRoute);
 router.use('/create-offer-project', createProjectRoute);
 router.use('/getTogether', getTogetherRoute);
-router.use('/quickResponse', authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER), quickResponseRouter);
-router.use('/message', authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER), handleMessageRoute);
+router.use(
+  '/quickResponse',
+  authenticateToken(
+    USER_ROLE.ADMIN,
+    USER_ROLE.SUPER_ADMIN,
+    USER_ROLE.SUB_ADMIN,
+    USER_ROLE.USER,
+  ),
+  quickResponseRouter,
+);
+router.use(
+  '/message',
+  authenticateToken(
+    USER_ROLE.ADMIN,
+    USER_ROLE.SUPER_ADMIN,
+    USER_ROLE.SUB_ADMIN,
+    USER_ROLE.USER,
+  ),
+  handleMessageRoute,
+);
 router.use('/bookMark', bookMarkRoute);
 router.use('/archive', archiveRoute);
 router.use('/role', authenticateSuperAdmin, handleRoleRoute);
 router.use('/notification', handleNotificationRoute);
 router.use('/order-message', handleOrderMessageRoute);
 router.post('/upload-image', uploadFile.any(), uploadImage);
-router.post("/contactForChat", authenticateToken(USER_ROLE.USER), startContact);
+router.post('/contactForChat', authenticateToken(USER_ROLE.USER), startContact);
 
 router.use('/order-status', getOrderStatusRoute);
-router.use('/order', OrderNoteRouter)
+router.use('/order', OrderNoteRouter);
 
-router.use('/find-order', findOrderRouter)
+router.use('/find-order', findOrderRouter);
 
-router.use('/requirement', RequirementSubmitRoute)
+router.use('/requirement', RequirementSubmitRoute);
 
 router.post(
-  "/sendMessageForChat/:user_id",
-  authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.SUPER_ADMIN),
-  sendMessageForChat
-)
+  '/sendMessageForChat/:user_id',
+  authenticateToken(
+    USER_ROLE.ADMIN,
+    USER_ROLE.SUB_ADMIN,
+    USER_ROLE.SUPER_ADMIN,
+  ),
+  sendMessageForChat,
+);
 
 router.use('/inbox', Start_Project_Controller);
 router.get('/avaiableforchat', chating.AvaiableForChat);
@@ -134,17 +167,21 @@ router.use('/multi-project', multiProjectRoute);
 
 //payment route
 router.post('/api/checkout-session', payment.stripePayment);
-router.post("/webhook", express.raw({ type: "application/json" }), payment.stripePayment);
+router.post(
+  '/webhook',
+  express.raw({ type: 'application/json' }),
+  payment.stripePayment,
+);
 
 router.post('/verify-email', async (req, res) => {
   try {
     const response = await AWS_SES.verifyEmailIdentity(req.body.email);
-    res.status(200).json({ message: "Email verification initiated", response });
+    res.status(200).json({ message: 'Email verification initiated', response });
   } catch (error) {
-    console.error("Error in /verify-email:", error);
+    console.error('Error in /verify-email:', error);
     res.status(500).json({
-      message: "Failed to verify email",
-      error: error || "Unknown error",
+      message: 'Failed to verify email',
+      error: error || 'Unknown error',
       details: error,
     });
   }
@@ -160,17 +197,14 @@ router.post('/send-email', async (req, res) => {
     // Send the email
     const response = await AWS_SES.sendEmail(email, subject, body);
     console.log(response, 'response');
-    res.status(200).json({ message: "Email sent successfully", response });
+    res.status(200).json({ message: 'Email sent successfully', response });
   } catch (error) {
-    console.error("Error in /send-email:", error);
+    console.error('Error in /send-email:', error);
     res.status(500).json({
-      message: "Failed to send email",
-      error: error || "Unknown error",
+      message: 'Failed to send email',
+      error: error || 'Unknown error',
       details: error,
     });
   }
 });
 export default router;
-
-
-
