b8c42872f3257cb728280b00907ee786353c7cfa
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-11-18 00:26:48 +0600

    update

--- src/modules/Review/Review.route.ts
+++ src/modules/Review/Review.route.ts
@@ -9,7 +9,7 @@ const reviewRouter = Router();
 reviewRouter.get("/get-all-owner-reviews", authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.SUPER_ADMIN), ReviewController.getAllOwnerReviews);
 
 reviewRouter.post("/create", authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER, USER_ROLE.SUPER_ADMIN), ReviewController.createReview);
-reviewRouter. ("/:orderId", authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER, USER_ROLE.SUPER_ADMIN), ReviewController.getReviewsByOrderId);
+reviewRouter.get("/:orderId", authenticateToken(USER_ROLE.ADMIN, USER_ROLE.SUB_ADMIN, USER_ROLE.USER, USER_ROLE.SUPER_ADMIN), ReviewController.getReviewsByOrderId);
 
 
 export default reviewRouter;
