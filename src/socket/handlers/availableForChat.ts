1b1473374a320886f7e32a0bfc4afb1490235cc4
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-11-25 02:24:20 +0600

    update logic

--- src/socket/handlers/availableForChat.ts
+++ src/socket/handlers/availableForChat.ts
@@ -4,26 +4,28 @@ import socketStore from "../socket-store";
 const availableForChat = (socket: Socket, io: any) => {
     socket.on("available-for-chat", (message) => {
         const onlineUsers = socketStore.getOnlineUsers();
+        const user = socket.user
 
-        // Check if the sender is an admin or a regular user
-        const sender = onlineUsers.find(user => user.socketId === socket.id);
 
-        if (!sender) {
+        // Get the target user's socket
+        const targetUserSocket = onlineUsers.find(user => user.userId === message.userId);
+        if (!targetUserSocket) {
             // If sender is not found in the online users, return
             return;
         }
 
         // If the sender is an admin (role check)
-        if (['ADMIN', 'SUB_ADMIN', 'SUPER_ADMIN'].includes(sender.role)) {
-            // Send to all admins
+        if (['ADMIN', 'SUB_ADMIN', 'SUPER_ADMIN'].includes(user.role)) {
+            const targetUserSocket = onlineUsers.find(user => user.userId === message.userId);
+            
+            if (targetUserSocket) {
+                io.to(targetUserSocket.socketId).emit('newChatMessage', message)
+            }
             const adminUserSockets = onlineUsers.filter(user => ['ADMIN', 'SUB_ADMIN', 'SUPER_ADMIN'].includes(user.role));
             adminUserSockets.forEach(adminSocket => {
                 io.to(adminSocket.socketId).emit("newChatMessage", message);
             });
-            const targetUserSocket = onlineUsers.find(user => user.userId === message.userId);
-            if (targetUserSocket) {
-                io.to(targetUserSocket.socketId).emit('newChatMessage', message)
-            }
+
         } else {
             // If the sender is a user, send the message to the target user
             const adminUserSockets = onlineUsers.filter(user => ['ADMIN', 'SUB_ADMIN', 'SUPER_ADMIN'].includes(user.role));
