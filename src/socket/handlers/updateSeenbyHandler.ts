9ad8f7fd778396e0b4dde1d2b787838c29786a0a
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-11-26 04:35:07 +0600

    fixed the all error of message seen

--- src/socket/handlers/updateSeenbyHandler.ts
+++ src/socket/handlers/updateSeenbyHandler.ts
@@ -1,24 +1,45 @@
-import { Socket } from "socket.io";
-import { prisma } from "../../libs/prismaHelper";
-
+import { Socket } from 'socket.io';
+import { prisma } from '../../libs/prismaHelper';
+import socketStore from '../socket-store';
 
 const updateSeenBy = (socket: Socket, io: any) => {
-    socket.on('seen', async (data) => {
-        const message = await prisma.message.updateMany({
-            where: {
-                OR: [
-                    { recipientId: data.userId as string },
-                    { senderId: data.userId as string },
-                ]
-            },
-            data: {
-                seenBy: {
-                    push: data.userId
-                },
-            },
-        });
+  socket.on('seen', async (data) => {
+    const onlineUsers = socketStore.getOnlineUsers();
 
-        io.emit('getSeenBy', message)
-    })
-}
+    const targetUserSocket = onlineUsers.find(
+      (user) => user.userId === data.userId,
+    );
+
+    console.log('targetUserSocket', targetUserSocket);
+
+    const message = await prisma.message.updateMany({
+      where: {
+        OR: [
+          { recipientId: data.userId as string },
+          { senderId: data.userId as string },
+        ],
+      },
+      data: {
+        seenBy: {
+          push: data.userId,
+        },
+        seen: true,
+      },
+    });
+
+    const messageData = await prisma.message.findMany({
+      where: {
+        OR: [
+          { recipientId: data?.recipientId as string },
+          { senderId: data?.recipientId as string },
+        ],
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    io.to(targetUserSocket?.socketId).emit('getSeenBy', messageData[0]);
+  });
+};
 export default updateSeenBy;
