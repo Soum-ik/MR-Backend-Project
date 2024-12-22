43782a9bcc3d9fd58e3ae72c777fe93304415275
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2024-12-23 00:58:14 +0600

    feat: update ExtendDelivery interface and add delivery date update utility

--- src/modules/Order_page/ExtendDelivery/ExtendDelivary.router.ts
+++ src/modules/Order_page/ExtendDelivery/ExtendDelivary.router.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { extendDelivery, approveExtensionRequest } from './ExtendDelivary.controller';
+import { ExtendDeliveryMessageOption, approveExtensionRequest } from './ExtendDelivary.controller';
 
 const router = express.Router();
 
 // Route to request an extension
-router.post('/order/extend-delivery', extendDelivery);
+router.post('/order/extend-delivery', ExtendDeliveryMessageOption);
 
 // Route to approve or reject an extension request
 router.post('/order/approve-extension', approveExtensionRequest);
