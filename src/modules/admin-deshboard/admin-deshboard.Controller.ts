c61264afcab3f3b0a2014bfd149d8cdded66bc4a
Stack Soumik. <110479389+Soum-ik@users.noreply.github.com>
2025-01-20 20:11:48 +0600

    update find order data cancel response@

--- src/modules/admin-deshboard/admin-deshboard.Controller.ts
+++ src/modules/admin-deshboard/admin-deshboard.Controller.ts
@@ -174,7 +174,7 @@ export const getOrderCount = catchAsync(
           },
         }),
         // Get canceled orders count
-        prisma.order.count({
+        prisma.order.findMany({
           where: {
             ...whereClause,
             projectStatus: ProjectStatus.CANCELED,
@@ -222,9 +222,14 @@ export const getOrderCount = catchAsync(
       ? totalEarnings / payments.length
       : 0;
 
+    const cancelOrders = {
+      length: canceledOrders.length,
+      price: canceledOrders.reduce((acc, payment) => acc + parseFloat(payment.totalPrice), 0),
+    }
+
     const totalOrder = {
       completedOrders,
-      canceledOrders,
+      canceledOrders: cancelOrders,
       totalEarnings,
       averageOrderValue,
       totalCancelledAmount,
